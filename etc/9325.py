001f4334f5040ec3b916692a0938cfd38b508453
Kyeong-soo ko <star77sa@naver.com>
2021-07-26 10:10:45 +0900

    Create 9325.py

---
+++ etc/9325.py
@@ -0,0 +1,11 @@
+# 9325 얼마?
+
+import sys
+T = int(input())
+for _ in range(T):
+    s = int(input())
+    n = int(input())
+    for _ in range(n):
+        q, p = map(int,input().split())
+        s += q*p
+    print(s)
