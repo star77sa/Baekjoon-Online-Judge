d3170a3c2d9e41bb1500f04f9cc087da1ebfbfc6
Kyeong-soo Ko <star77sa@naver.com>
2022-03-04 15:36:02 +0900

    ..

---
+++ 단계별로 풀어보기/3. for문/10871.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+	int N, X;
+	int A;
+
+	std::cin >> N >> X;
+
+	for (int i = 0; i < N; i++)
+	{
+		std::cin >> A;
+		if (A < X)
+		{
+			std::cout << A << " ";
+		}
+
+	}
+	return 0;
+}
